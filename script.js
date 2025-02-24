169e62083bc8140e4fdc476f41a7a26a7b893e02
rr-88 <rr1988@int.pl>
2025-02-24 06:24:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ form.addEventListener("submit", async function(event) {
         const formData = new FormData(form);
         const data = Object.fromEntries(formData);
         try {
-                const response = await fetch("https://script.google.com/macros/s/AKfycby-GRr-LR-1bQKocb0TwXRmjows5ipW2jTJJh_w8_WhS1spsf8fLh-pzgV23O9HWM6e/exec", {
+                const response = await fetch("https://script.google.com/macros/s/AKfycbygKKNX9bDUaTK65YcsLAfTNqt1QzGDWSeuJoo_xO-YV2YAsBiAPT0kz9anslcQbArL/exec", {
                         method: "POST",
                         body: new URLSearchParams(data)
                 });
