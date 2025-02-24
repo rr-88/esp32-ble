4a07a1264c39850794423d31ca643c125cdf0340
rr-88 <rr1988@int.pl>
2025-02-24 06:19:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ form.addEventListener("submit", async function(event) {
         const formData = new FormData(form);
         const data = Object.fromEntries(formData);
         try {
-                const response = await fetch("https://script.google.com/macros/s/AKfycbyf3Mtj6TqoPU1hfpdo8EUjOj6KyQdOzIgTit73GPmVfky2PLZhpjxdE2VQ-wQOs-4p/exec", {
+                const response = await fetch("https://script.google.com/macros/s/AKfycby-GRr-LR-1bQKocb0TwXRmjows5ipW2jTJJh_w8_WhS1spsf8fLh-pzgV23O9HWM6e/exec", {
                         method: "POST",
                         body: new URLSearchParams(data)
                 });
