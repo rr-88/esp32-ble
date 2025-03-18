9350dcc279b5915826fad502a7a6f0f6125a307c
rr-88 <rr1988@int.pl>
2025-03-18 06:13:13 +0100

    Update gs2.js

--- gs2.js
+++ gs2.js
@@ -15,7 +15,7 @@ function handleResponse(csvText) {
   let sheetObjects = csvToObjects(csvText);
   // sheetObjects is now an Array of Objects
   //console.log(sheetObjects);
-  var a = document.getElementById("decodedValue").innerText;
+  var a = document.getElementById("decodedValue").textContent;
   var b = sheetObjects.find(x => x.barcode_id == a).link;
   getLink(b);
     //console.log(data);
