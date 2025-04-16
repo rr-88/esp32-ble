069e1785295c72053fd928d15bf6923c1360f915
rr-88 <rr1988@int.pl>
2025-04-16 06:02:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ form.addEventListener("submit", async function(event) {
         event.preventDefault();
         const formData = new FormData(form);
         const data = Object.fromEntries(formData);
+        console.log(data);
         try {
                 const response = await fetch("https://script.google.com/macros/s/AKfycbwAm25DtDqBZVb5ehODBGcnkdS2HeE7jlBqRtd5islJ0-FwcTz5UgN4TP3uCe9C9Of3/exec", {
                         method: "POST",
