eaa7e6ce777c96317017d0c57635186098f3e9af
rr-88 <rr1988@int.pl>
2025-04-18 06:26:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ form.addEventListener("submit", async function(event) {
         const data = Object.fromEntries(formData);
         console.log(data);
         try {
-                const response = await fetch("https://script.google.com/macros/s/AKfycbzBjPH8Aj2EwHdHOy9OQgs8gXRUyo16ZXCq4cJ80vDnPO-QWMlgC9NluNV1v4YtufH1/exec", {
+                const response = await fetch("https://script.google.com/macros/s/AKfycbxTvS_bUlOrNJDQkLiEMoK-SnOcO2-bVkeCBI0z-68Pmwtzugj1VoLi1-HN8cmjnGCr/exec", {
                         method: "POST",
                         body: new URLSearchParams(data)
                 });
